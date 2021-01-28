87734f067f53e6cbafd95cf2a4b3d8c6d00eb0a1
aaron blanco alvarez <aaronba13@hotmail.com>
2021-01-28 20:24:28 +0100

    Creación proyecto.

---
+++ ModuloConversacionalPepper/translations/translation_es_ES.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hola</source>
+            <comment>Text</comment>
+            <translation>Hola</translation>
+        </message>
+    </context>
+</TS>
