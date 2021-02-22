441e4c2f4cc39c326449ce6d824a08c08cdc09ab
aaron blanco alvarez <aaronba13@hotmail.com>
2021-02-22 22:00:17 +0100

    Cambios en las cajas.

--- ModuloConversacionalPepper/translations/translation_es_ES.ts
+++ ModuloConversacionalPepper/translations/translation_es_ES.ts
@@ -64,12 +64,12 @@
         </message>
     </context>
     <context>
-        <name>behavior_1/behavior.xar:/Cerrar aplicacion</name>
+        <name>behavior_1/behavior.xar:/Close app</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>De acuerdo, vuelvo a mi estado anterior.</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">De acuerdo, vuelvo a mi estado anterior.</translation>
         </message>
     </context>
     <context>
@@ -80,55 +80,49 @@
             <translation type="obsolete">Hola</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>A la entrada del laboratorio podemos ver tanto el sensor de huellas dáctilares como el video portero automático como mecanismos de control de acceso. </source>
             <comment>Text</comment>
-            <translation type="unfinished">A la entrada del laboratorio podemos ver tanto el sensor de huellas dáctilares como el video portero automático como mecanismos de control de acceso. </translation>
+            <translation type="obsolete">A la entrada del laboratorio podemos ver tanto el sensor de huellas dáctilares como el video portero automático como mecanismos de control de acceso. </translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_aula</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Por último tenemos el aula, desde ahí podemos realizar el control domótico de la vivienda a través del controlador lógico.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Por último tenemos el aula, desde ahí podemos realizar el control domótico de la vivienda a través del controlador lógico.</translation>
+            <translation type="obsolete">Por último tenemos el aula, desde ahí podemos realizar el control domótico de la vivienda a través del controlador lógico.</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_cocina</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>En la cocina encontramos distintos electrodomésticos inteligentes como puede ser el frigorífico, la campana, el horno, la lavadora y el calentador. Además nos encontramos un sensor de CO2.</source>
             <comment>Text</comment>
-            <translation type="unfinished">En la cocina encontramos distintos electrodomésticos inteligentes como puede ser el frigorífico, la campana, el horno, la lavadora y el calentador. Además nos encontramos un sensor de CO2.</translation>
+            <translation type="obsolete">En la cocina encontramos distintos electrodomésticos inteligentes como puede ser el frigorífico, la campana, el horno, la lavadora y el calentador. Además nos encontramos un sensor de CO2.</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_dormitorio</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>En el dormitorio tenemos elementos habituales como una cama, armario o mesita de noche. La cama además está articulada y puede funcionar mediante comandos de voz con Alexa. Puedes probarlo después de esta presentación.</source>
             <comment>Text</comment>
-            <translation type="unfinished">En el dormitorio tenemos elementos habituales como una cama, armario o mesita de noche. La cama además está articulada y puede funcionar mediante comandos de voz con Alexa. Puedes probarlo después de esta presentación.</translation>
+            <translation type="obsolete">En el dormitorio tenemos elementos habituales como una cama, armario o mesita de noche. La cama además está articulada y puede funcionar mediante comandos de voz con Alexa. Puedes probarlo después de esta presentación.</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_salon</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>En el salón tenemos tanto una televisión inteligente como una cámara para reconocer al usuario.</source>
             <comment>Text</comment>
-            <translation type="unfinished">En el salón tenemos tanto una televisión inteligente como una cámara para reconocer al usuario.</translation>
+            <translation type="obsolete">En el salón tenemos tanto una televisión inteligente como una cámara para reconocer al usuario.</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_sensores</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Existen distintos sensores, algunos de ellos son de localización, cámaras y la estación meteorológica. Estos nos sirven para localizarnos de manera qué podemos encender o apagar las luces o conocer el tiempo que hace fuera. </source>
             <comment>Text</comment>
-            <translation type="unfinished">Existen distintos sensores, algunos de ellos son de localización, cámaras y la estación meteorológica. Estos nos sirven para localizarnos de manera qué podemos encender o apagar las luces o conocer el tiempo que hace fuera. </translation>
+            <translation type="obsolete">Existen distintos sensores, algunos de ellos son de localización, cámaras y la estación meteorológica. Estos nos sirven para localizarnos de manera qué podemos encender o apagar las luces o conocer el tiempo que hace fuera. </translation>
         </message>
     </context>
     <context>
@@ -147,19 +141,17 @@
     <context>
         <name>behavior_1/behavior.xar:/Explicacion_servicio</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Otra de las cosas que tenemos es un baño adaptado el cual está equipado con servicios esenciales como el inodoro, el plato de ducha o váscula. Adiccionalmente tenemos sensores de apertura y de presencia.</source>
             <comment>Text</comment>
-            <translation type="unfinished">Otra de las cosas que tenemos es un baño adaptado el cual está equipado con servicios esenciales como el inodoro, el plato de ducha o váscula. Adiccionalmente tenemos sensores de apertura y de presencia.</translation>
+            <translation type="obsolete">Otra de las cosas que tenemos es un baño adaptado el cual está equipado con servicios esenciales como el inodoro, el plato de ducha o váscula. Adiccionalmente tenemos sensores de apertura y de presencia.</translation>
         </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Finalizar presentacion</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Y con esto se finaliza la presentación, espero que te haya gustado. </source>
             <comment>Text</comment>
-            <translation type="unfinished">Y con esto se finaliza la presentación, espero que te haya gustado. </translation>
+            <translation type="obsolete">Y con esto se finaliza la presentación, espero que te haya gustado. </translation>
         </message>
     </context>
     <context>
@@ -173,19 +165,18 @@
     <context>
         <name>behavior_1/behavior.xar:/Preguntas</name>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>¿Tienes alguna duda?</source>
             <comment>Text</comment>
-            <translation type="unfinished">¿Tienes alguna duda?</translation>
+            <translation type="obsolete">¿Tienes alguna duda?</translation>
         </message>
     </context>
     <context>
-        <name>behavior_1/behavior.xar:/Presentacion</name>
+        <name>behavior_1/behavior.xar:/Presentation</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hola, soy Pepper y soy un robot con muchas utilidades, entre ellas, puedo presentar el laboratorio SmartHome, ¿Quieres que empiece?</source>
             <comment>Text</comment>
-            <translation type="unfinished"></translation>
+            <translation type="unfinished">Hola, soy Pepper y soy un robot con muchas utilidades, entre ellas, puedo presentar el laboratorio SmartHome, ¿Quieres que empiece?</translation>
         </message>
     </context>
     <context>
@@ -199,6 +190,123 @@
             <source>Hello</source>
             <comment>Text</comment>
             <translation type="vanished">Hola</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_Entrada</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>A la entrada del laboratorio podemos ver tanto el sensor de huellas dáctilares como el video portero automático como mecanismos de control de acceso. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">A la entrada del laboratorio podemos ver tanto el sensor de huellas dáctilares como el video portero automático como mecanismos de control de acceso. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_aula</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Por último tenemos el aula, desde ahí podemos realizar el control domótico de la vivienda a través del controlador lógico.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Por último tenemos el aula, desde ahí podemos realizar el control domótico de la vivienda a través del controlador lógico.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_cocina</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>En la cocina encontramos distintos electrodomésticos inteligentes como puede ser el frigorífico, la campana, el horno, la lavadora y el calentador. Además nos encontramos un sensor de CO2.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">En la cocina encontramos distintos electrodomésticos inteligentes como puede ser el frigorífico, la campana, el horno, la lavadora y el calentador. Además nos encontramos un sensor de CO2.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_dormitorio</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>En el dormitorio tenemos elementos habituales como una cama, armario o mesita de noche. La cama además está articulada y puede funcionar mediante comandos de voz con Alexa. Puedes probarlo después de esta presentación.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">En el dormitorio tenemos elementos habituales como una cama, armario o mesita de noche. La cama además está articulada y puede funcionar mediante comandos de voz con Alexa. Puedes probarlo después de esta presentación.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_salon</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>En el salón tenemos tanto una televisión inteligente como una cámara para reconocer al usuario.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">En el salón tenemos tanto una televisión inteligente como una cámara para reconocer al usuario.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_sensores</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Existen distintos sensores, algunos de ellos son de localización, cámaras y la estación meteorológica. Estos nos sirven para localizarnos de manera qué podemos encender o apagar las luces o conocer el tiempo que hace fuera. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Existen distintos sensores, algunos de ellos son de localización, cámaras y la estación meteorológica. Estos nos sirven para localizarnos de manera qué podemos encender o apagar las luces o conocer el tiempo que hace fuera. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Explicacion_servicio</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Otra de las cosas que tenemos es un baño adaptado el cual está equipado con servicios esenciales como el inodoro, el plato de ducha o váscula. Adiccionalmente tenemos sensores de apertura y de presencia.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Otra de las cosas que tenemos es un baño adaptado el cual está equipado con servicios esenciales como el inodoro, el plato de ducha o váscula. Adiccionalmente tenemos sensores de apertura y de presencia.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Finalizar presentacion</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Y con esto se finaliza la presentación, espero que te haya gustado. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Y con esto se finaliza la presentación, espero que te haya gustado. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Preguntas</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>¿Tienes alguna duda?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">¿Tienes alguna duda?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Preguntas (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>¿Tienes alguna duda?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">¿Tienes alguna duda?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Preguntas (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>¿Tienes alguna duda?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">¿Tienes alguna duda?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard Presentation/Preguntas (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>¿Tienes alguna duda?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">¿Tienes alguna duda?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Standard presentation</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>No soy capaz de averiguar tu edad así que usaremos la presentación estandar.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">No soy capaz de averiguar tu edad así que usaremos la presentación estandar.</translation>
         </message>
     </context>
 </TS>
