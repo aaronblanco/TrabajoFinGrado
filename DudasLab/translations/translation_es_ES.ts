24664c6bfcdcc1261bca764ee05ab1f8631ebe39
AARON BLANCO ALVAREZ <aba693@inlumine.ual.es>
2021-06-05 13:07:47 +0200

    Inicio complemento.

---
+++ DudasLab/translations/translation_es_ES.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hola</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hola</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Esto es una prueba.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Esto es una prueba.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hola</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hola</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prueba1</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prueba1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hola</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hola</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Prueba2</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Prueba2</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Apagar</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Apagar</translation>
+        </message>
+    </context>
+</TS>
